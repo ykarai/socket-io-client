476018e5a8fc981be8cffa44f13bf5aecdcbaa1a
ykarai <yos707@gmail.com>
2021-05-30 15:03:52 +0300

    ch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
 const ENDPOINT = "http://127.0.0.1:4001";
 //const ENDPOINT = "4001";
-
+//const ENDPOINT = "4001";
 
 function App() {
   const [response, setResponse] = useState("");
